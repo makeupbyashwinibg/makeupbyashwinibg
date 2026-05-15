2021fcf229917650974e876a152a0a5b68682972
makeupbyashwinibg <makeupbyashwinibg27@gmail.com>
2026-05-15 20:47:26 +0530

    changed the storebucket firebase

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
         apiKey: "AIzaSyDZV95F1YK7Bm_sug7Ah1P0c-BlkL3I5C0",
         authDomain: "makeup-by-ashwinibg.firebaseapp.com",
         projectId: "makeup-by-ashwinibg",
-        storageBucket: "makeup-by-ashwinibg.firebasestorage.app",
+        storageBucket: "makeup-by-ashwinibg.appspot.com",
         messagingSenderId: "260908975561",
         appId: "1:260908975561:web:76981e1bba93679f171418",
         measurementId: "G-GBWBRC46L8"
